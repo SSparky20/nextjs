745e038c9161a46147bb890cbd329a847706978e
zachr <zach.ryan1@icloud.com>
2023-11-07 19:44:46 -0700

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+"use client"
+import countButt from "./countButt"
+export default function page(){
+    return <div>
+        <countButt /><countButt />
+        <hr />
+        <a href="https://github.com">This is github</a>
+        <hr />
+        <p>search bar</p>
+    </div>
+}
