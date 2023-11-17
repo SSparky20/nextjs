88b621929d1e99b5210e69b545960be37b98ef1d
zachr <zach.ryan1@icloud.com>
2023-11-17 16:51:42 -0700

    new count

---
+++ app/CountB.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+
+export default function CountButt(props) {
+    let [count, setCount] = useState(0);
+
+    function handleClick() {
+        setCount(count + 1 * props.mult);
+    }
+
+    function rollOverCount() {
+        if (count > 10) {
+            setCount(0);
+        }
+    }
+
+    const buttonStyle = {
+        backgroundColor: "black",
+        color: "white",
+        borderRadius: "10px"
+    }
+    useEffect(rollOverCount, [count]);
+
+    return (
+        <div>
+            <button onClick={handleClick} style={buttonStyle}>{props.name}</button>
+            <div>{count}</div>
+        </div>
+    );
+}
+
+// love taco
--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,12 @@
 "use client"
-import CountButt from "./CountButt";
+import CountB from "./CountB";
 import SearchBar from "./SearchBar"
 
 export default function Page() {
     return (
         <div>
-            <CountButt name={"+1"} mult={1} />
-            <CountButt name={"+2"} mult={2} />
+            <CountB name={"+1"} mult={1} />
+            <CountB name={"+2"} mult={2} />
             <hr />
             <a href="https://github.com/SSparky20/nextjs">This is GitHub</a>
             <hr />
